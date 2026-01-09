6fae6f4e12dab76edbfeb5a394cc0959c53c6cd5
dxx <358110999@qq.com>
2026-01-09 18:52:26 +0800

    2

---
+++ ioc/pkg.go
@@ -0,0 +1,79 @@
+package ioc
+
+import (
+	"dxxproject/config_prepare/app_config"
+	"dxxproject/config_prepare/start_config"
+	"dxxproject/my/jwt_utils/jwt_user"
+	"dxxproject/my/my_logger"
+	"dxxproject/my/passwd_util"
+	"dxxproject/pkg/gorm_ok"
+	"dxxproject/pkg/nacos_ok"
+	"dxxproject/pkg/redis_ok"
+	"dxxproject/pkg/snowflake_ok"
+	"dxxproject/pkg/zap_ok"
+	"github.com/redis/go-redis/v9"
+	"github.com/samber/do/v2"
+	"gorm.io/gorm"
+)
+
+func injectPkg(injector do.Injector) {
+	//读取本地启动配置
+	do.Provide(injector, func(injector do.Injector) (*start_config.StartConfig, error) {
+		return start_config.NewStartConfig()
+	})
+
+	//nacos instance
+	do.Provide(injector, func(injector do.Injector) (*nacos_ok.NacosInstance, error) {
+		startConfig := do.MustInvoke[*start_config.StartConfig](injector)
+		return nacos_ok.NewNacosInstance(startConfig)
+	})
+
+	//app config
+	do.Provide(injector, func(injector do.Injector) (*app_config.AppConfig, error) {
+		instance := do.MustInvoke[*nacos_ok.NacosInstance](injector)
+		return app_config.NewAppConfig(instance)
+	})
+
+	//zap logger
+	do.Provide(injector, func(injector do.Injector) (*zap.Logger, error) {
+		appCfg := do.MustInvoke[*app_config.AppConfig](injector)
+		return zap_ok.NewZapLogger(appCfg)
+	})
+
+	//user jwt
+	do.Provide(injector, func(injector do.Injector) (*jwt_user.UserImpl, error) {
+		cfg := do.MustInvoke[*app_config.AppConfig](injector)
+		return jwt_user.NewJwtUserImpl(cfg)
+	})
+
+	// 雪花算法
+	do.Provide(injector, func(injector do.Injector) (*snowflake_ok.SnowflakeIMPL, error) {
+		startConfig := do.MustInvoke[*start_config.StartConfig](injector)
+		appConfig := do.MustInvoke[*app_config.AppConfig](injector)
+		return snowflake_ok.NewSnowFlake(startConfig, appConfig)
+	})
+
+	//密码工具
+	do.Provide(injector, func(injector do.Injector) (*passwd_util.PasswdUtilImpl, error) {
+		return passwd_util.NewPasswordUtil()
+	}) //密码加密
+
+	// 自定义 logger
+	do.Provide(injector, func(injector do.Injector) (*my_logger.MyLoggerZapImpl, error) {
+		zapLogger := do.MustInvoke[*zap.Logger](injector)
+		return my_logger.NewMyLogger(zapLogger)
+	})
+
+	// gorm db
+	do.Provide(injector, func(injector do.Injector) (*gorm.DB, error) {
+		appCfg := do.MustInvoke[*app_config.AppConfig](injector)
+		return gorm_ok.NewGormDb(appCfg)
+	}) //初始化gorm
+
+	// redis
+	do.Provide(injector, func(injector do.Injector) (*redis.Client, error) {
+		appCfg := do.MustInvoke[*app_config.AppConfig](injector)
+		return redis_ok.NewRedisClient(appCfg)
+	})
+
+}
